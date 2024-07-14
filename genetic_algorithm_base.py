74485ba769ba94af69d7b577fda5f3c2d2142036
Enzo Andreetto <enzo.andreetto@gmail.com>
2024-07-14 10:22:46 -0300

    Simplificar constructor

--- genetic_algorithm_base.py
+++ genetic_algorithm_base.py
@@ -4,10 +4,13 @@ from abc import ABC, abstractmethod
 
 class GeneticAlgorithmBase(ABC):
     
-    def __init__(
+    def __init__(self):
+        super().__init__()
+
+    def evolve(
             self,
-            population_size,
             chromosomes_size,
+            population_size,
             selection_type,
             crossover_type,
             num_generations,
@@ -15,57 +18,45 @@ class GeneticAlgorithmBase(ABC):
             mutation_prob=0.05,
             early_stop_fitness=None,
             verbose=False):
-        
-        super().__init__()
 
-        assert population_size > 0, 'population_size must be greater than 0'
         assert chromosomes_size > 0, 'population_size must be greater than 0'
+        assert population_size > 0, 'population_size must be greater than 0'
         assert selection_type is not None, 'selection_type is required'
         assert crossover_type is not None, 'crosover_type is required'
         assert crosssover_prob > 0, 'crosssover_prob must be greater than 0'
         assert mutation_prob > 0, 'mutation_prob must be greater than 0'
 
-        self._population_size = population_size
-        self._chromosomes_size = chromosomes_size
-        self._selection_type = selection_type
-        self._crossover_type = crossover_type
-        self._num_generations = num_generations
-        self._crossover_prob = crosssover_prob
-        self._mutation_prob = mutation_prob
-        self._early_stop_fitness = early_stop_fitness
-        self._verbose = verbose
+        population = np.random.randint(2, size=(population_size, chromosomes_size))
 
-    def evolve(self):
-        population = np.random.randint(2, size=(self._population_size, self._chromosomes_size))
-
-        for num_generation in np.arange(self._num_generations):
-            self._log_output(f'===============\nGeneration: {num_generation + 1}')
+        for num_generation in np.arange(num_generations):
+            self._log_output(f'===============\nGeneration: {num_generation + 1}', verbose)
 
             # Early stop if configured.
-            if self._early_stop_fitness is not None:
+            if early_stop_fitness is not None:
                 fittest_solution = next(
-                    (item for item in population if self._get_fitness(item) > self._early_stop_fitness),
+                    (item for item in population if self._get_fitness(item) > early_stop_fitness),
                     None
                 )
                 if fittest_solution is not None:
                     self._log_output(f'Early stopping. Fittest chromosome: {fittest_solution}. '
                                    + f'Fenotype: {self.get_fenotype(fittest_solution)}. '
-                                   + f'Aptitude: {self._get_fitness(fittest_solution)}')
+                                   + f'Aptitude: {self._get_fitness(fittest_solution)}',
+                                   verbose)
                     return fittest_solution
 
             # Selection.
-            parents = self._select_parents(population)
+            parents = self._select_parents(population, selection_type)
 
             # Crossover.
             descendents = []
             for i in np.arange(parents.shape[0] - 1, step=2):
-                descendent1, descendent2 = self._crossover(parents[i], parents[i + 1])
+                descendent1, descendent2 = self._crossover(parents[i], parents[i + 1], crossover_type, crosssover_prob)
                 descendents.extend([descendent1, descendent2])
 
             # Mutation.
             mutated_descendents = []
             for i, descendent in enumerate(descendents):
-                mutated_descendents.extend([self._mutate(descendent)])
+                mutated_descendents.extend([self._mutate(descendent, mutation_prob)])
             population = np.array(mutated_descendents)
 
             # Get the fittest.
@@ -73,7 +64,8 @@ class GeneticAlgorithmBase(ABC):
             fittest_solution = population[idx]
             self._log_output(f'Fittest chromosome: {fittest_solution}. '
                        + f'Fenotype: {self.get_fenotype(fittest_solution)}. '
-                       + f'Aptitude: {self._get_fitness(fittest_solution)}\n===============\n')
+                       + f'Aptitude: {self._get_fitness(fittest_solution)}\n===============\n',
+                       verbose)
 
         return fittest_solution
 
@@ -85,15 +77,15 @@ class GeneticAlgorithmBase(ABC):
     def _get_fitness(self, chromosome):
         pass
 
-    def _select_parents(self, population):
-        if self._selection_type == SelectionType.RWS:
+    def _select_parents(self, population, selection_type):
+        if selection_type == SelectionType.RWS:
             return self._select_parents_by_rws(population)
-        elif self._selection_type == SelectionType.TS:
+        elif selection_type == SelectionType.TS:
             return self._select_parents_by_ts(population)
-        elif self._selection_type == SelectionType.LRS:
+        elif selection_type == SelectionType.LRS:
             return self._select_parents_by_lrs(population)
         else:
-            raise Exception('Unknown selection type ', self._selection_type)
+            raise Exception('Unknown selection type ', selection_type)
 
     def _select_parents_by_rws(self, population):
         total_fitness = 0
@@ -134,18 +126,18 @@ class GeneticAlgorithmBase(ABC):
     def _select_parents_by_lrs(self, population):
         pass
 
-    def _crossover(self, chromosome1, chromosome2):
-        if self._crossover_type == CrossoverType.SinglePoint:
-            return self._crossover_single_point(chromosome1, chromosome2)
-        elif self._crossover_type == CrossoverType.TwoPoint:
+    def _crossover(self, chromosome1, chromosome2, crossover_type, crossover_prob):
+        if crossover_type == CrossoverType.SinglePoint:
+            return self._crossover_single_point(chromosome1, chromosome2, crossover_prob)
+        elif crossover_type == CrossoverType.TwoPoint:
             pass
-        elif self._crossover_type == CrossoverType.Uniform:
+        elif crossover_type == CrossoverType.Uniform:
             pass
         else:
-            raise Exception('Unknown crossover type ', self._crossover_type)
+            raise Exception('Unknown crossover type ', crossover_type)
         
-    def _crossover_single_point(self, chromosome1, chromosome2):
-        if np.random.uniform() < self._crossover_prob:
+    def _crossover_single_point(self, chromosome1, chromosome2, crossover_prob):
+        if np.random.uniform() < crossover_prob:
             crossover_point = np.random.randint(1, chromosome1.size - 1)
             return (
                 np.concat((chromosome1[:crossover_point], chromosome2[crossover_point:])),
@@ -153,14 +145,14 @@ class GeneticAlgorithmBase(ABC):
         else:
             return chromosome1, chromosome2
 
-    def _mutate(self, chromosome):
+    def _mutate(self, chromosome, mutation_prob):
         for idx, allele in enumerate(chromosome):
-            if np.random.random() < self._mutation_prob:
+            if np.random.random() < mutation_prob:
                 chromosome[idx] = 0 if allele == 1 else 1
         return chromosome
 
-    def _log_output(self, message):
-        if self._verbose == True:
+    def _log_output(self, message, verbose):
+        if verbose == True:
             print(message)
 
 class SelectionType(Enum):
@@ -178,15 +170,7 @@ if __name__ == '__main__':
     # Use case example: Minimize x^2.
     class GeneticX2(GeneticAlgorithmBase):
         def __init__(self):
-            super().__init__(
-                population_size=4,
-                chromosomes_size=5,
-                selection_type=SelectionType.RWS,
-                crossover_type=CrossoverType.SinglePoint,
-                num_generations=30,
-                mutation_prob=0.05,
-                early_stop_fitness=0.999,
-                verbose=True)
+            super().__init__()
             
         def get_fenotype(self, chromosome):
             return int(''.join(chromosome.astype(str)), 2)
@@ -195,4 +179,13 @@ if __name__ == '__main__':
             return 1 / (self.get_fenotype(chromosome) ** 2 + 1)
             
     genetic_x2 = GeneticX2()
-    solution = genetic_x2.evolve()
+
+    solution = genetic_x2.evolve(
+        population_size=4,
+        chromosomes_size=5,
+        selection_type=SelectionType.RWS,
+        crossover_type=CrossoverType.SinglePoint,
+        num_generations=30,
+        mutation_prob=0.05,
+        early_stop_fitness=0.999,
+        verbose=True)

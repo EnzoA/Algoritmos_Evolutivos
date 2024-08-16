2e8a6f5cc793d726eea174ed3b26932d921d32e9
jcrisarh <jcrisarh@gmail.com>
2024-08-16 15:43:47 -0300

    1 F

--- TP_3/ejercicio_1.py
+++ TP_3/ejercicio_1.py
@@ -107,3 +107,53 @@ print(f'\nLa solución óptima encontrada es ' +
       f'Su imagen es {objective_function(*[int(x) for x in gbest])}.')
 
 plot_gbests_by_iteration(gbest_by_iteration, num_particles)
+
+
+'''
+F. Realizar observaciones/comentarios/conclusiones en el .pdf acerca de qué cantidad mínima de partículas es reco-
+mendable utilizar para este problema específicamente.
+'''
+
+restriction_functions = [
+    lambda a, b, c, d: a >= 0.0 and b >= 0.0 and c >= 0.0 and d >= 0.0,
+    lambda a, b, c, d: 2.5 * a + 1.5 * b + 2.75 * c + 2.0 * d <= 640.0,
+    lambda a, b, c, d: 3.5 * a + 3.0 * b + 3.0 * c + 2.0 * d <= 960.0
+]
+
+particle_numbers = [2, 5, 6, 7, 8, 9, 10, 20, 50, 100, 200]
+
+results = []
+
+for num_particles in particle_numbers:
+    gbest, value, gbest_by_iteration = pso(
+        objective_function,
+        num_dimensions,
+        num_particles,
+        num_iterations,
+        c1,
+        c2,
+        w,
+        inferior_limit,
+        superior_limit,
+        optimization_criteria,
+        restriction_functions)
+
+    results.append({
+        'num_particles': num_particles,
+        'gbest': gbest,
+        'value': value
+    })
+
+    print(f'Número de partículas: {num_particles}, Mejor valor gbest: {value}, Solución: {gbest}')
+
+# Encontrar el mejor resultado
+best_result = max(results, key=lambda x: x['value'])
+
+print(f'\nEl mejor resultado global se obtuvo con {best_result["num_particles"]} partículas:')
+print(f'Solución: A={100 * int(best_result["gbest"][0])}, ' +
+      f'B={100 * int(best_result["gbest"][1])}, ' +
+      f'C={100 * int(best_result["gbest"][2])}, ' +
+      f'D={100 * int(best_result["gbest"][3])}.')
+print(f'Valor objetivo: {best_result["value"]}')
+
+
